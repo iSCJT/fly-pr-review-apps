9fc236ef79b20494018349853137e05e3a40bcad
Sean Thompson <80723794+iSCJT@users.noreply.github.com>
2024-10-23 21:21:33 +0100

    feat: add build secrets option

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,16 +36,23 @@ if [ "$EVENT_TYPE" = "closed" ]; then
   exit 0
 fi
 
+if [ -n "$INPUT_BUILD_SECRETS" ]; then
+  build_secrets=()
+  echo "$INPUT_BUILD_SECRETS" | tr ' ' '\n' | while IFS= read -r ARG; do
+    build_secrets+=("--build-secret $ARG")
+  done
+fi
+
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
   # Backup the original config file since 'flyctl launch' messes up the [build.args] section
   cp "$config" "$config.bak"
-  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org" $INPUT_LAUNCH_OPTIONS
+  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org" $INPUT_LAUNCH_OPTIONS ${build_secrets}
   # Restore the original config file
   cp "$config.bak" "$config"
 fi
 if [ -n "$INPUT_SECRETS" ]; then
-  echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
+  echo "$INPUT_SECRETS" | tr " " "\n" | flyctl secrets import --app "$app"
 fi
 
 # Attach postgres cluster to the app if specified.
@@ -56,9 +63,9 @@ fi
 # Trigger the deploy of the new version.
 echo "Contents of config $config file: " && cat "$config"
 if [ -n "$INPUT_VM" ]; then
-  flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE"
+  flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE" ${build_secrets}
 else
-  flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY"
+  flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY" ${build_secrets}
 fi
 
 # Make some info available to the GitHub workflow.
