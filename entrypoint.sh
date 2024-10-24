40146e4f713b50dc75142b4a30cea84876734f48
Sean Thompson <80723794+iSCJT@users.noreply.github.com>
2024-10-24 16:28:08 +0100

    feat: increase vm memory

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,7 +65,7 @@ echo "Contents of config $config file: " && cat "$config"
 if [ -n "$INPUT_VM" ]; then
   flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE" ${build_secrets}
 else
-  flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY" ${build_secrets}
+  flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "2gb" ${build_secrets}
 fi
 
 # Make some info available to the GitHub workflow.
